09a13d337120ff5af17f4897befae7a4c06c69e7
Caelan Desmond <ccdesmon@sundevils.asu.edu>
2024-08-30 02:29:15 -0700

    Add project files.

---
+++ Dual Destiny/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Dual_Destiny
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ErrorLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(183, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(323, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 174);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(146, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(183, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 25);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Control;
+            this.button4.Location = new System.Drawing.Point(274, 271);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(25, 25);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Control;
+            this.button5.Location = new System.Drawing.Point(361, 326);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(25, 25);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.Control;
+            this.button6.Location = new System.Drawing.Point(443, 285);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(25, 25);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.Control;
+            this.button7.Location = new System.Drawing.Point(525, 214);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(25, 25);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.Control;
+            this.button8.Location = new System.Drawing.Point(566, 141);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(25, 25);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.Control;
+            this.button9.Location = new System.Drawing.Point(546, 64);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(25, 25);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(169, 85);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(402, 244);
+            this.panel1.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(667, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(658, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Both Players Empty Boxes";
+            // 
+            // ErrorLabel
+            // 
+            this.ErrorLabel.AutoSize = true;
+            this.ErrorLabel.Location = new System.Drawing.Point(667, 152);
+            this.ErrorLabel.Name = "ErrorLabel";
+            this.ErrorLabel.Size = new System.Drawing.Size(29, 13);
+            this.ErrorLabel.TabIndex = 13;
+            this.ErrorLabel.Text = "Error";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ErrorLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Duel Destiny";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ErrorLabel;
+    }
+}
+
---
+++ Dual Destiny/Form1.cs
@@ -0,0 +1,470 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Runtime.Remoting.Messaging;
+using System.Collections.Concurrent;
+
+namespace Dual_Destiny
+{
+    public partial class Form1 : Form
+    {
+        bool[] One = { false, false };
+        bool[] two = { false, false };
+        bool[] three = { false, false };
+        bool[] four = { false, false };
+        bool[] five = { false, false };
+        bool[] six = { false, false };
+        bool[] seven = { false, false };
+        bool[] eight = { false, false };
+        bool[] nine = { false, false };
+
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.BackColor = Color.Transparent;
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void TurnOnOrangeLines()
+        {
+            Graphics g = panel1.CreateGraphics();
+            //g.Clear(Color.White);
+            Pen OrangePen = new Pen(Color.Orange);
+            OrangePen.Width = 10;
+            if (One[0])
+            {
+                g.DrawLine(OrangePen, 35, 0, 200, 135);
+            }
+            if (two[0])
+            {
+                g.DrawLine(OrangePen, 0, 65, 200, 135);
+            }
+            if (three[0])
+            {
+                g.DrawLine(OrangePen, 20, 145, 200, 135);
+            }
+            if (four[0])
+            {
+                g.DrawLine(OrangePen, 125, 200, 200, 135);
+            }
+            if (five[0])
+            {
+                g.DrawLine(OrangePen, 200, 300, 200, 135);
+            }
+            if (six[0])
+            {
+                g.DrawLine(OrangePen, 290, 220, 200, 135);
+            }
+            if (eight[0])
+            {
+                g.DrawLine(OrangePen, 365, 145, 200, 135);
+            }
+            if (eight[0])
+            {
+                g.DrawLine(OrangePen, 400, 65, 200, 135);
+            }
+            if (nine[0])
+            {
+                g.DrawLine(OrangePen, 385, 0, 200, 135);
+            }
+        }
+        private void TurnOnGreenLines()
+        {
+            Graphics g = panel1.CreateGraphics();
+            //g.Clear(Color.White);
+            Pen GreenPen = new Pen(Color.White);
+            GreenPen.Width = 10;
+            if (One[1])
+            {
+                g.DrawLine(GreenPen, 35, 0, 200, 135);
+            }
+            if (two[1])
+            {
+                g.DrawLine(GreenPen, 0, 65, 200, 135);
+            }
+            if (three[1])
+            {
+                g.DrawLine(GreenPen, 20, 145, 200, 135);
+            }
+            if (four[1])
+            {
+                g.DrawLine(GreenPen, 125, 200, 200, 135);
+            }
+            if (five[1])
+            {
+                g.DrawLine(GreenPen, 200, 300, 200, 135);
+            }
+            if (six[1])
+            {
+                g.DrawLine(GreenPen, 290, 220, 200, 135);
+            }
+            if (seven[1])
+            {
+                g.DrawLine(GreenPen, 365, 145, 200, 135);
+            }
+            if (eight[1])
+            {
+                g.DrawLine(GreenPen, 400, 65, 200, 135);
+            }
+            if (nine[1])
+            {
+                g.DrawLine(GreenPen, 385, 0, 200, 135);
+            }
+        }
+        private void TurnOffLines()
+        {
+            Graphics g = panel1.CreateGraphics();
+            //g.Clear(Color.White);
+            Pen WhitePen = new Pen(Color.Green);
+            WhitePen.Width = 10;
+            if (One[1])
+            {
+                g.DrawLine(WhitePen, 35, 0, 200, 135);
+            }
+            if (two[1])
+            {
+                g.DrawLine(WhitePen, 0, 65, 200, 135);
+            }
+            if (three[1])
+            {
+                g.DrawLine(WhitePen, 20, 145, 200, 135);
+            }
+            if (four[1])
+            {
+                g.DrawLine(WhitePen, 125, 200, 200, 135);
+            }
+            if (five[1])
+            {
+                g.DrawLine(WhitePen, 200, 300, 200, 135);
+            }
+            if (six[1])
+            {
+                g.DrawLine(WhitePen, 290, 220, 200, 135);
+            }
+            if (seven[1])
+            {
+                g.DrawLine(WhitePen, 365, 145, 200, 135);
+            }
+            if (eight[1])
+            {
+                g.DrawLine(WhitePen, 400, 65, 200, 135);
+            }
+            if (nine[1])
+            {
+                g.DrawLine(WhitePen, 385, 0, 200, 135);
+            }
+        }
+        private void clearNumbers()
+        {
+            One[0] = false;
+            two[0] = false;
+            three[0] = false;
+            four[0] = false;
+            five[0] = false;
+            six[0] = false;
+            seven[0] = false;
+            eight[0] = false;
+            nine[0] = false;
+            One[1] = false;
+            two[1] = false;
+            three[1] = false;
+            four[1] = false;
+            five[1] = false;
+            six[1] = false;
+            seven[1] = false;
+            eight[1] = false;
+            nine[1] = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isOrange = One[0];
+            bool isGreen = One[1];
+            if(isGreen)
+            {
+                One[0] = false;
+                One[1] = false;
+            }
+            else if (isOrange)
+            {
+                One[1] = true;
+            }
+            else
+            {
+                One[0] = true;
+            }
+            TurnOnOrangeLines();
+            TurnOnGreenLines();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool isOrange = two[0];
+            bool isGreen = two[1];
+            if (isGreen)
+            {
+                two[0] = false;
+                two[1] = false;
+            }
+            else if (isOrange)
+            {
+                two[1] = true;
+            }
+            else
+            {
+                two[0] = true;
+            }
+            TurnOnOrangeLines();
+            TurnOnGreenLines();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bool isOrange = three[0];
+            bool isGreen = three[1];
+            if (isGreen)
+            {
+                three[0] = false;
+                three[1] = false;
+            }
+            else if (isOrange)
+            {
+                three[1] = true;
+            }
+            else
+            {
+                three[0] = true;
+            }
+            TurnOnOrangeLines();
+            TurnOnGreenLines();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bool isOrange = four[0];
+            bool isGreen = four[1];
+            if (isGreen)
+            {
+                four[0] = false;
+                four[1] = false;
+            }
+            else if (isOrange)
+            {
+                four[1] = true;
+            }
+            else
+            {
+                four[0] = true;
+            }
+            TurnOnOrangeLines();
+            TurnOnGreenLines();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            bool isOrange = five[0];
+            bool isGreen = five[1];
+            if (isGreen)
+            {
+                five[0] = false;
+                five[1] = false;
+            }
+            else if (isOrange)
+            {
+                five[1] = true;
+            }
+            else
+            {
+                five[0] = true;
+            }
+            TurnOnOrangeLines();
+            TurnOnGreenLines();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            bool isOrange = six[0];
+            bool isGreen = six[1];
+            if (isGreen)
+            {
+                six[0] = false;
+                six[1] = false;
+            }
+            else if (isOrange)
+            {
+                six[1] = true;
+            }
+            else
+            {
+                six[0] = true;
+            }
+            TurnOnOrangeLines();
+            TurnOnGreenLines();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            bool isOrange = seven[0];
+            bool isGreen = seven[1];
+            if (isGreen)
+            {
+                seven[0] = false;
+                seven[1] = false;
+            }
+            else if (isOrange)
+            {
+                seven[1] = true;
+            }
+            else
+            {
+                seven[0] = true;
+            }
+            TurnOnOrangeLines();
+            TurnOnGreenLines();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            bool isOrange = eight[0];
+            bool isGreen = eight[1];
+            if (isGreen)
+            {
+                eight[0] = false;
+                eight[1] = false;
+            }
+            else if (isOrange)
+            {
+                eight[1] = true;
+            }
+            else
+            {
+                eight[0] = true;
+            }
+            TurnOnOrangeLines();
+            TurnOnGreenLines();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            bool isOrange = nine[0];
+            bool isGreen = nine[1];
+            if (isGreen)
+            {
+                nine[0] = false;
+                nine[1] = false;
+            }
+            else if (isOrange)
+            {
+                nine[1] = true;
+            }
+            else
+            {
+                nine[0] = true;
+            }
+            TurnOnOrangeLines();
+            TurnOnGreenLines();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string Text = textBox1.Text;
+            bool isParsed = int.TryParse(Text, out int result);
+            if (Text.Length == 6 && isParsed)
+            {
+                int[] unmatchedNums = { Text[0], Text[1], Text[2], Text[3], Text[4], Text[5] };
+                clearNumbers();
+
+                ErrorLabel.Text = "";
+                foreach (int n in unmatchedNums)
+                {
+                    //ErrorLabel.Text += n;
+                    //n = n - 48;
+                    switch (n)
+                    {
+                        case 49:
+                            One[0] = true;
+                            break;
+                        case 50:
+                            two[0] = true;
+                            break;
+                        case 51:
+                            three[0] = true;
+                            break;
+                        case 52:
+                            four[0] = true;
+                            break;
+                        case 53:
+                            five[0] = true;
+                            break;
+                        case 54:
+                            six[0] = true;
+                            break;
+                        case 55:
+                            seven[0] = true;
+                            break;
+                        case 56: 
+                            eight[0] = true; 
+                            break;
+                        case 57: 
+                            nine[0] = true;
+                            break;
+                    }
+                }
+                if (!One[0]) 
+                {
+                    One[1] = true;
+                }
+                if (!two[0])
+                {
+                    two[1] = true;
+                }
+                if (!three[0])
+                {
+                    three[1] = true;
+                }
+                if (four[0])
+                {
+                    four[1] = true;
+                }
+                if (five[0])
+                { 
+                    five[1] = true;
+                }
+                if (six[0])
+                {
+                    six[1] = true;
+                }
+                if (seven[0])
+                {
+                    seven[1] = true;
+                }
+                if (eight[0])
+                {
+                    eight[1] = true;
+                }
+                if (nine[0])
+                {
+                    nine[1] = true;
+                }
+
+                TurnOnGreenLines();
+            }
+            else
+            {
+                clearNumbers();
+                Graphics g = panel1.CreateGraphics();
+                g.Clear(Color.White);
+            }
+        }
+    }
+}
